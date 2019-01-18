8c52e8e164cda79c3d1ae8013aef6023959fa219
nameslash <36557446+nameslash@users.noreply.github.com>
2019-01-18 13:59:40 +0700

    กหฟ
    ดห

--- blog/resources/views/template.blade.php
+++ blog/resources/views/template.blade.php
@@ -15,7 +15,7 @@
 <body>
     
         <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom fixed-top" >
-                <a class="navbar-icon" href="http://127.0.0.1:8000/"><img src="{{url('/images/UPLogo.png')}}" alt="" style="width: auto ; height: 35px;"></a>
+                <a class="navbar-icon" href="{{ URL('/') }}"><img src="{{url('/images/UPLogo.png')}}" alt="" style="width: auto ; height: 35px;"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
@@ -23,10 +23,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                      <a class="nav-link" href="http://127.0.0.1:8000/">ระบบสารบัญเอกสาร <span class="sr-only">(current)</span></a>
-                    </li>   
-                    <li class="nav-item">
-                      <a class="nav-link" href="http://127.0.0.1:8000/list"><i class="fas fa-align-left"></i> &nbsp; ดูรายการเอกสาร</a> 
+                      <a class="nav-link" href="{{ URL('/') }}">ระบบสารบัญเอกสาร <span class="sr-only">(current)</span></a>
+                    </li>                  <li class="nav-item">
+                      <a class="nav-link" href="{{ URL('/list') }}"><i class="fas fa-align-left"></i> &nbsp; ดูรายการเอกสาร</a> 
+                    </li>   <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-address-card"></i> &nbsp; จัดการเอกสาร
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ URL('/') }}">&nbsp;ธุรการรับ</a> 
+                        <a class="dropdown-item" href="{{ URL('/') }}">&nbsp;ธุรการส่ง</a>
+                       
+                      </div>
                     </li>
                     <li class="nav-item dropdown">
                       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
