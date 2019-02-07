db2ffa49058b7f29dfe0de4219f81ee8cc902bd8
Wolftime <sirachutg@gmail.com>
2019-02-07 13:16:05 +0700

    FIX BUG IN CREATE AND DATATABLES

--- blog/resources/views/documents/index.blade.php
+++ blog/resources/views/documents/index.blade.php
@@ -185,35 +185,20 @@
 @endif
 
 <div class="table-responsive-lg table-hover container " style="margin-top: 70px">
-        @include('documents.mcreate')
+
+    <div class="row">
+        <div>
+            <h1 style="line-height: 0.7;">ตารางแสดงรายการบันทึกข้อความ &nbsp;</h1>
+        </div>
+
+        <div>
+            <a class="btn btn-success" href="{{ route('documents.create') }}"> Create New Product</a>
+
+        </div>
+    </div>
 
 
-    <form>
-            <div class="row">
-                <div class="col-sm-8">
-                    <h1 style="line-height: 0.7;">ตารางแสดงรายการบันทึกข้อความ &nbsp;</h1>
-                </div>
-                <div class="col-sm-4">
-
-                        {{-- @include('documents.mcreate') --}}
-
-                </div>
-
-            </div>
         <table class="table" id="table">
-
-                    <div class="row">
-                        {{-- <div>
-                            <a class="btn btn-success" href="{{ route('documents.create') }}"> Create New Product</a>
-                        </div> --}}
-
-                        <div class="col-sm-5">
-                            {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                            <i class="fas fa-plus-square"></i> &nbsp; เพิ่มรายการ</button> --}}
-
-                        </div>
-                    </div>
-
             <thead>
                 <tr >
                     <th>#</th>
@@ -263,7 +248,7 @@
                 @endforeach
             </tbody>
         </table>
-    </form>
+
 </div>
 
 <script>
