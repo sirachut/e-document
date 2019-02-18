f29541a9d4a2d1319d54deca20b7e452a807b456
Wolftime <sirachutg@gmail.com>
2019-02-18 10:14:23 +0700

    FIX CSS && JAVASCRIPT

--- blog/resources/views/documents/index.blade.php
+++ blog/resources/views/documents/index.blade.php
@@ -4,187 +4,13 @@
 
 @section('content')
 
-
-
-
-<!-- Modal -->
-{{-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-
-        <div class="modal-content">
-
-            <div class="modal-header">
-
-                <h5 class="modal-title" id="exampleModalLabel"> <i class="fas fa-plus-square"></i> &nbsp; เพิ่มบันทึกข้อความ</h5>
-
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-
-            </div>
-
-            <div class="modal-body">
-
-                <form class="form-horizontal" method="POST" action="{{ URL('/document') }}">
-                    <!-- if there are creation errors, they will show here -->
-                    @if($errors->all())
-                        <ul class="has-error">
-                    @foreach ($errors->all() as $message)
-                            <li>{{ $message }}</li>
-                    @endforeach
-                        </ul>
-                    @endif
-
-                    {{ csrf_field() }} --}}
-
-                    <!-- Material input -->
-
-{{--
-                    <form>
-                            <div class="form-row">
-                                <div class="form-group col-md-7">
-                                    <label for="FACULTY_NAME_TH">หน่วยงาน</label>
-
-                    <select class="form-control">
-                            <option selected>กรุณาเลือกหน่วยงาน...</option>
-
-                                    @foreach($Faculty as $key => $value)
-                                        <option>{{ $value->FACULTY_NAME_TH }}</option>
-                                    @endforeach
-                                    </select>
-
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <label for="faculty_department">ฝ่ายงาน</label>
-                                    <input type="text" class="form-control" id="faculty_department" placeholder="ฝ่ายงานของหน่วยงาน">
-                                </div>
-                                <div class="form-group col-md-2">
-                                    <label for="faculty_tel">โทร.</label>
-                                    <input type="text" class="form-control" id="faculty_tel" placeholder="xxxx" maxlength="4">
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="DOCUMENT_ST_NUMBER">ที่</label>
-                                    <input type="text" class="form-control" id="DOCUMENT_ST_NUMBER" placeholder="เลขที่ ศธ.">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="DOCUMENT_DATEIN">วันที่</label>
-
-                                    <input id="DOCUMENT_DATEIN" class="form-control">
-                                    <script>
-                                        $('#DOCUMENT_DATEIN').datepicker({
-                                            uiLibrary: 'bootstrap4',
-                                        });
-                                    </script>
-
-                                </div>
-                            </div>
-                            <div class="form-row">
-                              <div class="form-group col-md-6">
-                                <label for="DOCUMENT_NAME">เรื่อง</label>
-                                <input type="text" class="form-control" id="DOCUMENT_NAME">
-                              </div>
-                              <div class="form-group col-md-4">
-                                <label for="inputState">State</label>
-                                <select id="inputState" class="form-control">
-                                  <option selected>Choose...</option>
-                                  <option>...</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="gridCheck">
-                                <label class="form-check-label" for="gridCheck">
-                                  Check me out
-                                </label>
-                              </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary" style="float:right">Sign in</button>
-                          </form> --}}
-
-
-                    {{-- <div class="form-group{{ $errors->has('DOCUMENT_NUMBER') ? ' has-error' : '' }}">
-                        <label for="title" class="col-md-4 control-label">เลข Barcode* : </label>
-                        <!-- Material input -->
-
-                        <div class="col-md-6">
-
-                            <input id="DOCUMENT_NUMBER" type="text" class="form-control" name="DOCUMENT_NUMBER" value="{{ old('DOCUMENT_NUMBER') }}" required autofocus>
-
-                            @if ($errors->has('title'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('DOCUMENT_NUMBER') }}</strong>
-                                </span>
-                            @endif
-
-                        </div>
-
-                    </div>
-
-                    <div class="form-group{{ $errors->has('DOCUMENT_PRIORITY') ? ' has-error' : '' }}">
-                        <label for="title" class="col-md-4 control-label">ประเภทเอกสาร* : </label>
-
-                        <div class="col-md-6">
-                            <input id="DOCUMENT_PRIORITY" type="text" class="form-control" name="DOCUMENT_PRIORITY" value="{{ old('DOCUMENT_PRIORITY') }}" required autofocus>
-
-                            @if ($errors->has('title'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('DOCUMENT_PRIORITY') }}</strong>
-                                </span>
-                            @endif
-
-                        </div>
-                    </div>
-
-                    <div class="form-group{{ $errors->has('DOCUMENT_ST_NUMBER') ? ' has-error' : '' }}">
-                        <label for="content" class="col-md-4 control-label">เลขที่ ศธ.  *</label>
-
-                        <div class="col-md-6">
-                            <input id="DOCUMENT_ST_NUMBER" type="text" class="form-control" name="DOCUMENT_ST_NUMBER" value="{{ old('DOCUMENT_PRIORITY') }}" required autofocus>
-                            @if ($errors->has('DOCUMENT_ST_NUMBER'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('DOCUMENT_ST_NUMBER') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                สร้าง
-                            </button>
-                        </div>
-                    </div>
-
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-
-        </div>
-    </div>
-</div> --}}
-
-{{-- <div>
-    @php
-        $userdata = Session::get('userdata');
-        echo $userdata['username'];
-    @endphp
-</div> --}}
-
 @if ($message = Session::get('success'))
         <div class="alert alert-success">
             <p>{{ $message }}</p>
         </div>
 @endif
 
-<div class="table-responsive-lg table-hover container " style="margin-top: 70px">
+<div class="table-responsive-lg table-hover container " style="margin-top: 70px;padding-bottom:100px">
 
     <div class="row">
         <div>
@@ -192,7 +18,7 @@
         </div>
 
         <div>
-            {{-- <a class="btn btn-success" href="{{ route('documents.create') }}"> Create New Product</a> --}}
+
             @include('documents.create')
 
         </div>
@@ -203,11 +29,9 @@
             <thead>
                 <tr >
                     <th>#</th>
-
                     <th>เลขที่ ศธ.</th>
                     <th>หัวรื่อง</th>
                     <th>วันที่รับเข้า</th>
-
                     <th>Action</th>
                 </tr>
             </thead>
@@ -224,18 +48,17 @@
                         <td>{{ $value->DOCUMENT_ST_NUMBER }}</td>
                         <td style="width: 40%">{{ $value->DOCUMENT_NAME }}</td>
                         <td>
-                                @php
-                                    echo App\Http\Controllers\DocumentController::DateThai($value->DATE_IN);
-                                @endphp
+                            @php
+                                echo App\Http\Controllers\DocumentController::DateThai($value->DATE_IN);
+                            @endphp
                         </td>
                         <td>
-
 
                             <form class="form-horizontal" method="POST" action="{{ route('documents.destroy',$value->DOCUMENT_ID) }}">
 
                                 <a class="btn btn-xs btn-success" href="{{ route('documents.show',$value->DOCUMENT_ID) }}">Show</a>
-                                <a class="btn btn-xs btn-success" href="{{ URL::to('documentitem/' . $value->DOCUMENT_ID) }}">Show 2</a>
 
+                                <a class="btn btn-xs btn-success" href="{{ URL::to('documentitem/' . $value->DOCUMENT_ID) }}">Show 2</a>
 
                                 <a class="btn btn-xs btn-info" href="{{ route('documents.edit',$value->DOCUMENT_ID) }}">Edit</a>
 
@@ -254,15 +77,16 @@
 
 </div>
 
+
 <script>
     $(document).ready(function() {
-    $('#table').DataTable( {
-        "bFilter" : false,
-        "bLengthChange": false,
-        "searching": true,
+        $('#table').DataTable( {
+            "bFilter" : false,
+            "bLengthChange": false,
+            "searching": true,
 
+        });
     } );
-} );
 
     $( "#form_action_del" ).submit(function( event ) {
         if(confirm("ต้องการลบข้อมูลหรือไม่!")){
--- blog/resources/views/layouts/style.blade.php
+++ blog/resources/views/layouts/style.blade.php
@@ -1,31 +1,25 @@
+{{-- GOOGLE FONT --}}
+<link href="https://fonts.googleapis.com/css?family=Sarabun" rel="stylesheet">
 
-    <link href="https://fonts.googleapis.com/css?family=Sarabun" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/bootstrap.css">
+{{-- ICONAWESOME --}}
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-    <link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+{{-- BOOTSTRAP4 --}}
+<link rel="stylesheet" href="/css/bootstrap.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/color.css">
+{{-- PONG --}}
+<link rel="stylesheet" href="/css/animate.css">
+<link rel="stylesheet" href="/css/color.css">
 
-  
+{{-- DATATABLE --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+<script type='text/javascript' src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type='text/javascript' src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script type='text/javascript' src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
-
-{{--
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.0/js/mdb.min.js"></script> --}}
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 
